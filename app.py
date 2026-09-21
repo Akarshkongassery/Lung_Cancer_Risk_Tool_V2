e9d2b0518888df1723a4e6e7c6a4abf8c5a4f431
Akarshkongassery <76961442+Akarshkongassery@users.noreply.github.com>
2026-09-21 12:09:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -188,11 +188,11 @@ class CPRDVARHAModelAdapter:
     checkpoint_path: Path,
     calibrator_path: Path,
     calibration_metadata_path: Path,):
-    self.spec = spec
-    self.checkpoint_path = Path(checkpoint_path)
-    self.calibrator_path = Path(calibrator_path)
-    self.calibration_metadata_path = Path(
-        calibration_metadata_path
+        self.spec = spec
+        self.checkpoint_path = Path(checkpoint_path)
+        self.calibrator_path = Path(calibrator_path)
+        self.calibration_metadata_path = Path(
+            calibration_metadata_path
     )
 
         # -----------------------------------------------
